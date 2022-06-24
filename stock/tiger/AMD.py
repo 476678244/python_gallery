774e7dd7aa16e22eea2fa786b6be570e4302e072
Zonghan Wu <a476678244@163.com>
2022-06-24 22:52:09 +0800

    find_price

--- stock/tiger/AMD.py
+++ stock/tiger/AMD.py
@@ -13,10 +13,28 @@ april_buys = [b0404, b0406, b0408]
 june_buys = [b0617]
 
 compute_avg_cost(all_buys, 'all_buys AMD')
-compute_avg_cost(april_buys, 'april_buys AMD')
+april_avg, num, cost = compute_avg_cost(april_buys, 'april_buys AMD')
 compute_avg_cost(june_buys, 'june_buys AMD')
 
 close_price = get_close_price('AMD')
+print('latest_price is : ')
+print(close_price)
 compute_buys_income(all_buys, close_price, 'all_buys AMD')
 compute_buys_income(june_buys, close_price, 'june_buys AMD')
-print(close_price)
+
+
+# num * (april_avg - close_price) <= compute_buys_income(june_buys, close_price, 'june_buys AMD') - 1.99
+
+# (compute_buys_income(june_buys, close_price, 'june_buys AMD')- 1.99)/ num + close_price >= april_avg
+
+
+def find_price(rescue_num, latest_price, rescue_buys, threshold):
+    tmp_price = latest_price
+    while (compute_buys_income(rescue_buys, tmp_price) - 1.99) / rescue_num + tmp_price < threshold:
+        tmp_price = tmp_price + 0.1
+    return tmp_price
+
+
+for n in [1, 2, 3, 4]:
+    print('if want to rescue : {} , price should be : '.format(n))
+    print(find_price(n, close_price, june_buys, april_avg))
