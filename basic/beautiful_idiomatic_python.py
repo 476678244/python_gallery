c0bc04b2252ddf85f134465479cedd19f37c2df3
Zonghan Wu <a476678244@163.com>
2022-08-24 21:59:45 +0800

    beautiful_idiomatic_python

---
+++ basic/beautiful_idiomatic_python.py
@@ -0,0 +1,7 @@
+# https://gist.github.com/0x4D31/f0b633548d8e0cfb66ee3bea6a0deff9
+for i in range(6):
+    print(i ** 2)
+
+colors = ['red', 'green', 'blue', 'yellow']
+for color in reversed(colors):
+    print(color)
